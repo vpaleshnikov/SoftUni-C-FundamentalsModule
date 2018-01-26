be622f7eb36bdf1a29fcf51a26411c5f6f178f38
vpaleshnikov <vpaleshnikov@gmail.com>
2018-01-26 23:04:04 +0200

    Adding new exercises

---
+++ C#Advanced/MultidimensionalArrays-Exercise/01.MatrixOfPalindromes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.MatrixOfPalindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = input[0];
+            var cols = input[1];
+
+            var matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = "" + (char)('a' + row) + (char)('a' + row + col) + (char)('a' + row);
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Advanced/MultidimensionalArrays-Exercise/02.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                { 
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            var primaryDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        primaryDiagonalSum += matrix[row + i, col + i];
+                    }
+                    break;                    
+                }
+                break;
+            }
+
+            var secondaryDiagonalSum = 0;
+            for (int row = n - 1 ; row > 0; row--)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        secondaryDiagonalSum += matrix[row - i, col + i];
+                    }
+                    break;
+                }
+                break;
+            }
+
+            var diff = Math.Abs(primaryDiagonalSum + secondaryDiagonalSum);
+            Console.WriteLine(diff);
+        }
+    }
+}
