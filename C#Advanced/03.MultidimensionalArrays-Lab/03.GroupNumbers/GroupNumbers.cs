bce902dfa45742d10857fea16bac72760e70556e
vpaleshnikov <vpaleshnikov@gmail.com>
2018-01-26 23:03:28 +0200

    Adding new exercises

--- C#Advanced/03.MultidimensionalArrays-Lab/03.GroupNumbers/GroupNumbers.cs
+++ C#Advanced/03.MultidimensionalArrays-Lab/03.GroupNumbers/GroupNumbers.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace _03.GroupNumbers
 {
@@ -8,34 +7,13 @@ namespace _03.GroupNumbers
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine()
+            var numbers = Console.ReadLine()
                 .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-            
-            var firstResult = new List<int>();
-            var secondResult = new List<int>();
-            var thirdResult = new List<int>();
+                .Select(int.Parse);
 
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (Math.Abs(input[i]) % 3 == 0)
-                {
-                    firstResult.Add(input[i]);
-                }
-                if (Math.Abs(input[i]) % 3 == 1)
-                {
-                    secondResult.Add(input[i]);
-                }
-                if (Math.Abs(input[i]) % 3 == 2)
-                {
-                    thirdResult.Add(input[i]);
-                }
-            }
-
-            Console.WriteLine(string.Join(" ",firstResult));
-            Console.WriteLine(string.Join(" ", secondResult));
-            Console.WriteLine(string.Join(" ", thirdResult));
+            Console.WriteLine(string.Join(" ", numbers.Where(n => Math.Abs(n) % 3 == 0)));
+            Console.WriteLine(string.Join(" ", numbers.Where(n => Math.Abs(n) % 3 == 1)));
+            Console.WriteLine(string.Join(" ", numbers.Where(n => Math.Abs(n) % 3 == 2)));
         }
     }
 }
