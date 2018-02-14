2daaff76d91cd09b9b836c88ff53edba8dffa372
vpaleshnikov <vpaleshnikov@gmail.com>
2018-02-14 18:38:57 +0200

    Adding new exercises

---
+++ C#OOPBasics/02.DefiningClasses-Exercise/10.CarSalesman/Car.cs
@@ -0,0 +1,18 @@
+﻿public class Car
+{
+    public string Model { get; set; }
+
+    public string Engine { get; set; }
+
+    public string Weight { get; set; }
+
+    public string Color { get; set; }
+
+    public Car(string model, string engine)
+    {
+        this.Model = model;
+        this.Engine = engine;
+        this.Weight = "n/a";
+        this.Color = "n/a";
+    }
+}
---
+++ C#OOPBasics/02.DefiningClasses-Exercise/10.CarSalesman/CarSalesman.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CarSalesman
+{
+    public static void Main()
+    {
+        var enginesCount = int.Parse(Console.ReadLine());
+
+        var engines = new List<Engine>();
+
+        for (int engineIndex = 0; engineIndex < enginesCount; engineIndex++)
+        {
+            var engineInfo = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var engineModel = engineInfo[0];
+            var enginePower = engineInfo[1];
+
+            var engine = new Engine(engineModel, enginePower);
+
+            if (engineInfo.Length == 3)
+            {
+                if (engineInfo[2].Length == 1)
+                {
+                    engine.Efficiency = engineInfo[2];
+                }
+                else
+                {
+                    engine.Displacement = engineInfo[2];
+                }
+            }
+            else if (engineInfo.Length > 3)
+            {
+                engine.Displacement = engineInfo[2];
+                engine.Efficiency = engineInfo[3];
+            }
+
+            engines.Add(engine);
+        }
+
+        var numberOfCars = int.Parse(Console.ReadLine());
+
+        var cars = new List<Car>();
+
+        for (int carIndex = 0; carIndex < numberOfCars; carIndex++)
+        {
+            var carsInfo = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var carModel = carsInfo[0];
+            var carEngine = carsInfo[1];
+
+            var car = new Car(carModel, carEngine);
+
+            if (carsInfo.Length == 3)
+            {
+                int number;
+                if (int.TryParse(carsInfo[2], out number))
+                {
+                    car.Weight = carsInfo[2];
+                }
+                else
+                {
+                    car.Color = carsInfo[2];
+                }
+            }
+            else if (carsInfo.Length > 3)
+            {
+                car.Weight = carsInfo[2];
+                car.Color = carsInfo[3];
+            }
+
+            cars.Add(car);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model}:");
+            foreach (var engine in engines)
+            {
+                if (car.Engine == engine.Model)
+                {
+                    Console.WriteLine($"  {engine.Model}:");
+                    Console.WriteLine($"    Power: {engine.Power}");
+                    Console.WriteLine($"    Displacement: {engine.Displacement}");
+                    Console.WriteLine($"    Efficiency: {engine.Efficiency}");
+                }
+            }
+            Console.WriteLine($"  Weight: {car.Weight}");
+            Console.WriteLine($"  Color: {car.Color}");
+        }
+    }
+}
